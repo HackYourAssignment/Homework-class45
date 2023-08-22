1c31c2adfdfc8ac52ea1fbcdc4ad17c26950ba14
kumait Mustafa <kumait@gmail.com>
2023-08-22 22:10:33 +0200

    Almost

--- 1-JavaScript/Week2/homework/ex3-tellFortune.js
+++ 1-JavaScript/Week2/homework/ex3-tellFortune.js
@@ -4,25 +4,6 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
 
 Why pay a fortune teller when you can just program your fortune yourself?
 
-1. Create four arrays, `numKids`, `partnerNames`, `locations` and `jobTitles`. 
-   Give each array five random values that have to do with the name of 
-   the variable.
-
-2. Complete the function `selectRandomly`. This function should take an array 
-   as a parameter and return a randomly selected element as its return value.
-
-3. Complete the function named `tellFortune` as follows:
-
-   - It should take four arguments (in the order listed): 
-     * the array with the options for the number of children, 
-     * the array with the options for the partner's name, 
-     * the array with the options for the geographic location and 
-     * the array with the options for the job title.
-   - It should use the `selectRandomly` function to randomly select values from 
-     the arrays.
-   - It should return a string: "You will be a `jobTitle` in `location`, 
-    married to `partnerName` with `numKids` kids."
-
 4. Call the function three times, passing the arrays as arguments. Use `
    console.log` to display the results.
 
@@ -30,38 +11,40 @@ Note: The DRY principle is put into practice here: instead of repeating the code
 randomly select array elements four times inside the `tellFortune` function 
 body, this code is now written once only in a separated function.
 -----------------------------------------------------------------------------*/
+function selectRandomly(takesArray) {
+const random = Math.floor(Math.random() * takesArray.length);
+return takesArray[random];
+};
 
-// This function should take an array as its parameter and return
-// a randomly selected element as its return value.
-function selectRandomly(/* TODO parameter(s) go here */) {
-  // TODO complete this function
-}
+function tellFortune(numKids, partnerNames, locations, jobTitles) {
 
-function tellFortune(/* TODO add parameter(s) here */) {
-  // TODO complete this function
+  const jobTitle = selectRandomly(jobTitles);
+  const location = selectRandomly(locations);
+  const partnerName = selectRandomly(partnerNames);
+  const numKid = selectRandomly(numKids);
+  
+  return `You will be a ${jobTitle} in ${location}, married to ${partnerName} with ${numKid} kids.`;
 }
 
 function main() {
   const numKids = [
-    // TODO add elements here
+    'only son', 2, 'twins' , 3, 4
   ];
-
-  const partnerNames = [
-    // TODO add elements here
-  ];
-
   const locations = [
-    // TODO add elements here
+    'Amsterdam', 'LA', 'London', 'Tokyo', 'Paris'
   ];
-
   const jobTitles = [
-    // TODO add elements here
+    'web developer', 'IT Engineer', 'Coder', 'Programmer', 'Software Developer'
   ];
-
+  const partnerNames = {
+    m: ['Kees','Ken', 'Koen', 'Kai', 'Kris' ],
+    f: ['Ka', 'Kyra', 'Kim', 'Kaat', 'Salma Hayek'] 
+ };
   console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
   console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
   console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
-}
+};
+main();
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
