db2a6db47275596003f312e93405a412a2eb1940
kumait Mustafa <kumait@gmail.com>
2023-08-23 15:22:01 +0200

    Sync

--- 1-JavaScript/Week2/homework/redoScope-ex3.js
+++ 1-JavaScript/Week2/homework/redoScope-ex3.js
@@ -9,7 +9,14 @@ const partnerNames = {
   m: ['Kees', 'Ken', 'Koen', 'Kai', 'Kris'],
 };
 
-const partnerAny = partnerNames.m.concat(partnerNames.f);
+// ... The spread operator :
+/* const numbers = [1, 2, 3];
+const string = ['fruit', 'vegetable'];
+const combined = [...numbers, ...string];
+console.log(combined) // Will be [1, 2, 3, 'fruit', 'vegetable'] */ //HYF-book:
+
+// //const partnerAny = partnerNames.m.concat(partnerNames.f);
+const partnerAny = [...partnerNames.m, ...partnerNames.f];
 
 const locations = ['Amsterdam', 'LA', 'London', 'Tokyo', 'Paris'];
 
@@ -20,17 +27,23 @@ const jobTitles = [
   'Programmer',
   'Software Developer',
 ];
-
-function selectRandomly(takesArray) {
+// IN IN Arrow Function:
+// //function selectRandomly(takesArray) {
+//   //const random = Math.floor(Math.random() * takesArray.length);
+//   //return takesArray[random];
+// //}
+const selectRandomly = (takesArray) => {
   const random = Math.floor(Math.random() * takesArray.length);
-  return takesArray[random];
-}
+  takesArray[random];
+};
 
+// Destructure the partnerNames object:
 function partner(feMale, partnerNames) {
+  const { m, f } = partnerNames; // Do this
   if (feMale === 'b') {
-    return selectRandomly(partnerNames.m);
+    return selectRandomly(m); // instead of: `partnerNames.m`
   } else if (feMale === 'g') {
-    return selectRandomly(partnerNames.f);
+    return selectRandomly(f);
   } else if (feMale === 'any') {
     return selectRandomly(partnerAny);
   } else {
@@ -53,3 +66,21 @@ function main() {
   console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
 }
 main();
+/*
+ex1-giveCompliment.js   ex3-tellFortune.js      ex5-shoppingCartPure.js ex7-mindPrivacy.js
+ex2-dogYears.js         ex4-shoppingCart.js     ex6-totalCost.js        redoScope-ex3.js
+kumait@Kumaits-MBP homework % node redoScope-ex3.js
+/Users/kumait/Documents/Homework-class45/1-JavaScript/Week2/homework/redoScope-ex3.js:1
+const feMale = prompt(
+               ^
+
+ReferenceError: prompt is not defined
+    at Object.<anonymous> (/Users/kumait/Documents/Homework-class45/1-JavaScript/Week2/homework/redoScope-ex3.js:1:16)
+    at Module._compile (node:internal/modules/cjs/loader:1256:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1310:10)
+    at Module.load (node:internal/modules/cjs/loader:1119:32)
+    at Module._load (node:internal/modules/cjs/loader:960:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+    at node:internal/main/run_main_module:23:47
+
+Node.js v18.17.1   */
