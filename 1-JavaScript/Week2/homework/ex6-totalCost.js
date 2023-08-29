7e30848e9aba7b46dc1a4c5f307510633505e9cb
liz-garcia <lizgarciamarin@gmail.com>
2023-08-29 19:17:53 +0200

    Finished assignments for week 2 JavaScript

--- 1-JavaScript/Week2/homework/ex6-totalCost.js
+++ 1-JavaScript/Week2/homework/ex6-totalCost.js
@@ -1,42 +1,37 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-6-total-cost-is
 
-You want to buy a couple of things from the supermarket to prepare for a party.
-After scanning all the items the cashier wants to give you the total price, but
-the machine is broken! Let's write her a function that does it for her
-instead!
-
-1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
-
-2. Complete the function called `calculateTotalPrice`.
-
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
-
-3. Complete the unit test functions and verify that all is working as expected.
------------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  beers: 3.0,
+  chips: 1.5,
+  cheese: 1.5,
+  orangeJuice: 2.0,
+  chiliSauce: 2.0,
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(cart) {
+  const cartPrices = Object.values(cart);
+  let amount = 0;
+
+  cartPrices.forEach((price) => {
+    amount += price;
+  });
+
+  return `Total: €${amount}`;
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  const expected = 1;
+  const actual = calculateTotalPrice.length;
+  console.assert(expected === actual);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  const expected = 'Total: €10';
+  const actual = calculateTotalPrice(cartForParty);
+  console.assert(expected === actual);
 }
 
 function test() {
