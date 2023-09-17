c53ca584609b2e1b4296980cccc766fb53d4b14f
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2023-09-17 11:20:37 +0200

    kadir-bozkurt-browser-week1

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -1,14 +1,26 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-4-whats-the-time
 
-1. Inside the `index.js`, complete the `addCurrentTime` to add the current time 
-  to the webpage. Make sure it's written in the HH:MM:SS notation (hour, minute,
-  second). Use `setInterval()` to make sure the time stays current.
-2. Have the function execute when it's loading in the browser.
-------------------------------------------------------------------------------*/
+const watchElement = document.createElement('h1');
+const bodyElement = document.querySelector('body');
+
+watchElement.className = 'watch';
+bodyElement.appendChild(watchElement);
+
 function addCurrentTime() {
-  // TODO complete this function
+  const now = new Date();
+
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+
+  console.log(currentTime);
+
+  watchElement.textContent = currentTime;
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(addCurrentTime, 1000);
+  addCurrentTime();
+});
--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,28 +1,34 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
 
-1. Create a variable to store a reference to the `<img>` element.
-2. Change the style of the `<img>` to have a `left` of `0px`, so that it starts 
-   at the left hand of the screen.
-3. Complete the function called catWalk() to move the cat 10 pixels to the right
-   of where it started, by changing the `left` style property.
-4. Call that function every 50 milliseconds. Your cat should now be moving 
-   across the screen from left to right. Hurrah!
-5. When the cat reaches the right-hand of the screen, restart them at the left 
-   hand side (`0px`). So they should keep walking from left to right across the 
-   screen, forever and ever.
-6. When the cat reaches the middle of the screen, replace the img with an image 
-   of a cat dancing (use this URL given below), keep it dancing for 5 seconds, 
-   and then replace the img with the original image and have it 
-   continue the walk.
+const catElement = document.querySelector('img');
+let startPoint = 0;
+const maxWidth = window.innerWidth;
+const middleScreen = Math.round((maxWidth - catElement.width) / 2 / 10) * 10;
 
-   Dancing cat URL:
+let dancing = false;
 
-   https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
------------------------------------------------------------------------------*/
 function catWalk() {
-  // TODO complete this function
+  if (!dancing) {
+    catElement.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+    startPoint += 10;
+    catElement.style.left = `${startPoint}px`;
+  }
+
+  if (startPoint === middleScreen) {
+    catElement.src =
+      'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    dancing = true;
+    setTimeout(() => {
+      dancing = false;
+    }, 5000);
+  }
+
+  if (startPoint >= maxWidth) {
+    startPoint = 0;
+  }
 }
 
-// TODO execute `catWalk` when the browser has completed loading the page
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(catWalk, 50);
+  catWalk();
+});
