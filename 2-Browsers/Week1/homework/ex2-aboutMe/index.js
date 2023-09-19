720936889363cfce3e0efe26165876bec0007ccd
liz-garcia <lizgarciamarin@gmail.com>
2023-09-19 21:43:34 +0200

    Finished assignments for week1 Browsers

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -1,12 +1,16 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-2-about-me
 
-1. Using JavaScript, replace each of the spans (`nickname`, fav-food`, 
-   `hometown`) with your own information.
-2. In JavaScript, iterate through each `<li>` and change the class to 
-   `list-item`.
-3. Look in the css file!
-------------------------------------------------------------------------------*/
+const nickname = document.getElementById('nickname');
+const favFood = document.getElementById('fav-food');
+const hometown = document.getElementById('hometown');
 
-// TODO add your JavaScript code here.
+nickname.textContent = 'Liz Luz';
+favFood.textContent = 'Cheesecake';
+hometown.textContent = 'Managua, Nicaragua';
+
+const ulElement = document.querySelector('ul');
+const liElements = ulElement.querySelectorAll('li');
+
+liElements.forEach((liElement) => {
+  liElement.setAttribute('class', 'list-item');
+});
--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,28 +1,43 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
 
-1. Create a variable to store a reference to the `<img>` element.
-2. Change the style of the `<img>` to have a `left` of `0px`, so that it starts 
-   at the left hand of the screen.
-3. Complete the function called catWalk() to move the cat 10 pixels to the right
-   of where it started, by changing the `left` style property.
-4. Call that function every 50 milliseconds. Your cat should now be moving 
-   across the screen from left to right. Hurrah!
-5. When the cat reaches the right-hand of the screen, restart them at the left 
-   hand side (`0px`). So they should keep walking from left to right across the 
-   screen, forever and ever.
-6. When the cat reaches the middle of the screen, replace the img with an image 
-   of a cat dancing (use this URL given below), keep it dancing for 5 seconds, 
-   and then replace the img with the original image and have it 
-   continue the walk.
+const imgElement = document.querySelector('img');
+imgElement.style.left = '0px';
 
-   Dancing cat URL:
-
-   https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
------------------------------------------------------------------------------*/
 function catWalk() {
-  // TODO complete this function
+  const initialLeft = parseInt(imgElement.style.left, 10);
+  
+  const setNewLeft = () => {
+    const newLeft = `${initialLeft + 10}px`;
+    imgElement.style.left = newLeft;
+  };
+
+  const resetWalkingCat = () => {
+    const walkingCat =
+      'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+    imgElement.src = walkingCat;
+    setNewLeft();
+    catWalkInterval = setInterval(catWalk, 50);
+  };
+
+  const screenWidth = window.innerWidth;
+  const screenMiddle = screenWidth / 2;
+  const imgWidth = imgElement.naturalWidth;
+  const imgMiddle = imgWidth / 2;
+
+  if (initialLeft >= screenWidth) {
+    imgElement.style.left = '0px';
+  } else if (initialLeft === screenMiddle) {
+    imgElement.style.left = '50%';
+    imgElement.style.marginLeft = `-${imgMiddle}px`;
+    clearInterval(catWalkInterval);
+    imgElement.src =
+      'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    setTimeout(resetWalkingCat, 5000);
+  } else {
+    setNewLeft();
+  }
 }
 
-// TODO execute `catWalk` when the browser has completed loading the page
+let catWalkInterval = setInterval(catWalk, 50);
+
+window.onload = catWalk;
