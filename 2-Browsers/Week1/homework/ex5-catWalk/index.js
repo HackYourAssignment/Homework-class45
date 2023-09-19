65ee203e411678c1ae5adcfe2d995587a09f06ad
sanaz <sanazzarei1995@gmail.com>
2023-09-19 15:03:25 +0200

    browsers homeworks are added

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,28 +1,32 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
-
-1. Create a variable to store a reference to the `<img>` element.
-2. Change the style of the `<img>` to have a `left` of `0px`, so that it starts 
-   at the left hand of the screen.
-3. Complete the function called catWalk() to move the cat 10 pixels to the right
-   of where it started, by changing the `left` style property.
-4. Call that function every 50 milliseconds. Your cat should now be moving 
-   across the screen from left to right. Hurrah!
-5. When the cat reaches the right-hand of the screen, restart them at the left 
-   hand side (`0px`). So they should keep walking from left to right across the 
-   screen, forever and ever.
-6. When the cat reaches the middle of the screen, replace the img with an image 
-   of a cat dancing (use this URL given below), keep it dancing for 5 seconds, 
-   and then replace the img with the original image and have it 
-   continue the walk.
-
-   Dancing cat URL:
-
-   https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
------------------------------------------------------------------------------*/
+const imgElement = document.getElementsByTagName('img')[0];
+let catPosition = 0;
+const screenWidth = window.innerWidth;
+const middleOfScreen = screenWidth / 2;
+imgElement.style.left = `${catPosition}px`;
 function catWalk() {
-  // TODO complete this function
+  catPosition += 10;
+  imgElement.style.left = `${catPosition}px`;
+  if (catPosition === middleOfScreen) {
+    clearInterval(catInterval);
+    catDance();
+    setTimeout(() => {
+      imgElement.src =
+        'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+      imgElement.alt = 'cat walking';
+      catInterval = setInterval(catWalk, 50);
+    }, 5000);
+  }
+  if (catPosition > screenWidth) {
+    catPosition = 0;
+  }
 }
+function catDance() {
+  imgElement.src =
+    'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  imgElement.alt = 'cat is dancing';
+}
+let catInterval = setInterval(catWalk, 50);
 
 // TODO execute `catWalk` when the browser has completed loading the page
+document.addEventListener('DOMContentLoaded', catWalk);
