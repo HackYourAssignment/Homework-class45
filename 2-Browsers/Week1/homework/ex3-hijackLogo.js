c2b54f34005ea5531dbb2a2786b2facae41e53be
liz-garcia <lizgarciamarin@gmail.com>
2023-09-19 21:52:40 +0200

    Corrected ex3-hijackLogo

--- 2-Browsers/Week1/homework/ex3-hijackLogo.js
+++ 2-Browsers/Week1/homework/ex3-hijackLogo.js
@@ -2,7 +2,7 @@
 
 function hijackGoogleLogo() {
   document.querySelector('body');
-  const googleLogo = document.getElementsById('hplogo');
+  const googleLogo = document.getElementById('hplogo');
   googleLogo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
   googleLogo.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg 1x';
 }
