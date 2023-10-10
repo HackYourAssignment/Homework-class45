7c002b858454eb2edc4e71082df7d28db80ad5ca
Enes <snggenes@gmail.com>
2023-10-10 21:54:13 +0200

    tests done

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -1,47 +1,26 @@
 'use strict';
 
-const { set } = require('lodash');
-
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-UsingAPIs/Week1#exercise-1-john-who
-
-Rewrite this function, but replace the callback syntax with the Promise syntax:
-- Have the `getAnonName` function return a `new Promise`.
-- If the Promise `resolves`, pass the full name as an argument to resolve with.
-- If the Promise `rejects`, pass an error as the argument to reject with: "You 
-  didn't pass in a first name!"
-------------------------------------------------------------------------------*/
-// TODO see above
-const getAnonName = (firstName, callback) => {
+const getAnonName = (firstName) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!firstName) {
         reject(new Error("You didn't pass in a first name!"));
       }
-      const fullName = `${firstName} Doe`;
-      callback(fullName);
-      resolve();
+      resolve(`${firstName} Doe`);
     }, 1000);
   });
 };
-// const getAnonName = (firstName, callback) => {
-//   setTimeout(() => {
-//     if (!firstName) {
-//       callback(new Error("You didn't pass in a first name!"));
-//       return;
-//     }
-
-//     const fullName = `${firstName} Doe`;
-
-//     callback(fullName);
-//   }, 1000);
-// };
 
 function main() {
-  getAnonName('John', console.log);
+  getAnonName('John')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error.massage);
+    });
 }
 
-// ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -1,15 +1,5 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-UsingAPIs/Week1#exercise-3-roll-a-die
 
-- Run the unmodified program and confirm that problem described occurs.
-- Refactor the `rollDie()` function from callback-based to returning a
-  promise.
-- Change the calls to `callback()` to calls to `resolve()` and `reject()`.
-- Refactor the code that call `rollDie()` to use the promise it returns.
-- Does the problem described above still occur? If not, what would be your
-  explanation? Add your answer as a comment to be bottom of the file.
-------------------------------------------------------------------------------*/
 function rollDie() {
   return new Promise((resolve, reject) => {
     const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
