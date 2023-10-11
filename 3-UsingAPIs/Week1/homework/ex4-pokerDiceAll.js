fc2cfcca040abed7d5be8def1ffe8c1b5d475a0b
Rama <120380006+rama-website@users.noreply.github.com>
2023-10-11 16:41:30 +0200

    adding the answer in the end of the file

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -50,3 +50,22 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+//You may also notice that, in the case of a rejected promise, dice that have not yet finished their roll continue to do so. Can you explain why? Please add your answer as a comment to the end of the exercise file.
+/*
+1.Asynchronous Nature: JavaScript, especially in the browser,
+ is inherently asynchronous.
+  When we initiate a promise (in this case, rolling a die),
+   it runs independently in the background while the main program continues to execute.
+
+   2.Event Loop: JavaScript uses an event loop to manage asynchronous operations. It continuously checks the message queue for tasks that need to be executed.
+    When a promise is resolved or rejected, it adds a task to the queue to handle the appropriate callback (either `then` or `catch`) when the promise is settled.
+
+    3.Non-Blocking: Promises are non-blocking, which means that the rejection of one promise doesn't block the execution of other promises. 
+    If one promise is rejected, the event loop doesn't halt; it continues to execute other tasks, including subsequent promises.
+
+
+    In the context of rolling dice in the exercise, if one die rolls off the table and its promise is rejected, it doesn't prevent the other dice from completing their rolls. The rejected promise is handled asynchronously, and the subsequent dice rolls continue to execute their promises and resolve as expected.
+
+So, even if one die has a problem (rejects), the other dice that haven't encountered issues continue to roll, and their promises are resolved independently. This is a fundamental characteristic of asynchronous programming in JavaScript.
+*/
