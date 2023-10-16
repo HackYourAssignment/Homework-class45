1ccf02129e035687ca233824d73f93f1e6a7b940
Lenin Del Río <142052112+Lenindelrionicaragua@users.noreply.github.com>
2023-10-16 16:19:04 +0200

    Update ex2-checkDoubleDigits.js

--- 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
@@ -3,34 +3,34 @@
 function checkDoubleDigits(number) {
   return new Promise((resolve, reject) => {
 
-    if (number >= 10 && number <= 99) {
-      resolve("This is a double digit number!");
-       } else {
-        reject(new Error(`Expected a double digit number but got ${number}`));
-       }
-  });
+  if (number >= 10 && number <= 99) {
+    resolve("This is a double digit number!");
+  } else {
+    reject(new Error(`Expected a double digit number but got ${number}`));
+  }
+});
 }
 
 function main() {
-  checkDoubleDigits(9) // should reject
+  checkDoubleDigits(9)
     .then((message) => console.log(message))
     .catch((error) => console.log(error.message));
 
-  checkDoubleDigits(10) // should resolve
+  checkDoubleDigits(10) 
     .then((message) => console.log(message))
     .catch((error) => console.log(error.message));
 
-  checkDoubleDigits(99) // should resolve
+  checkDoubleDigits(99) 
     .then((message) => console.log(message))
     .catch((error) => console.log(error.message));
 
-  checkDoubleDigits(100) // should reject
+  checkDoubleDigits(100) 
     .then((message) => console.log(message))
     .catch((error) => console.log(error.message));
 }
 
-// ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
+
 module.exports = checkDoubleDigits;
