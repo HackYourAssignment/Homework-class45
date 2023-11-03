a91398c7b04248d9afd63af255f2ff4c14e176ae
Kumait <137179507+29Kumait@users.noreply.github.com>
2023-11-03 01:50:40 +0100

    complete Homework Using APIs Week 2

--- 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
+++ 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
@@ -28,12 +28,18 @@ function addTableRow(table, label, value) {
   createAndAppend('td', tr, { text: value });
 }
 
-function renderLaureate(ul, { knownName, birth, death }) {
+function renderLaureate(ul, laureate) {
+  console.log(laureate);
+  const { knownName, birth, death } = laureate;
   const li = createAndAppend('li', ul);
   const table = createAndAppend('table', li);
   addTableRow(table, 'Name', knownName.en);
-  addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString}`);
-  addTableRow(table, 'Death', `${death.date}, ${death.place.locationString}`);
+  if (birth) {
+    addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString}`);
+  }
+  if (death) {
+    addTableRow(table, 'Death', `${death.date}, ${death.place.locationString}`);
+  }
 }
 
 function renderLaureates(laureates) {
@@ -43,7 +49,7 @@ function renderLaureates(laureates) {
 
 async function fetchAndRender() {
   try {
-    const laureates = getData(
+    const { laureates } = await getData(
       'https://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
     );
     renderLaureates(laureates);
